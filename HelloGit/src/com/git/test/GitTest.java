a88149dd7519564b571f6cd7418a528fa60b7f9c
Siva <kotapallisivakesava@gmail.com>
2019-03-15 15:15:51 +0530

    We Added String names

--- HelloGit/src/com/git/test/GitTest.java
+++ HelloGit/src/com/git/test/GitTest.java
@@ -3,10 +3,13 @@ package com.git.test;
 public class GitTest {
 
 	public static void main(String[] args) {
+		String[] string = {"Siva","Prasanna"};
+		System.out.println(string.length);
 		System.out.println("Hello Git");
 		System.out.println("We are in Git");
 		System.out.println("Git Repo");
 		System.out.println("Hi");
+		
 	}
 
 }
