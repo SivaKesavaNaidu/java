4edc8486e95a75a4a95ab7fba17a4aea9aed71b4
Siva <kotapallisivakesava@gmail.com>
2019-03-15 14:49:53 +0530

    We Add Two System

--- HelloGit/src/com/git/test/GitTest.java
+++ HelloGit/src/com/git/test/GitTest.java
@@ -4,6 +4,8 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
+		System.out.println("We are in Git");
+		System.out.println("Git Repo");
 	}
 
 }
